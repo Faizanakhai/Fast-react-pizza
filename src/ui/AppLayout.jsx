f2cbd480dd580aa90252f77ea877a5f28b785f18
Faizan Akhai <125543208+Faizanakhai@users.noreply.github.com>
2025-01-14 11:48:19 +0530

    Add files via upload

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import Header from "./Header";
+import CartOverview from "../Features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+
+      <div className="overflow-auto scroll-smooth">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
